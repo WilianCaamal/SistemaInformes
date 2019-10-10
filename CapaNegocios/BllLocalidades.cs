6ba3ee5d0dbcdf61f241cfa228bb850591a8c426
Wilian Caamal <wiliancaamal31@gmail.com>
2019-10-09 23:22:18 -0500

    Listar localidades por id municipio

---
+++ CapaNegocios/BllLocalidades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using CapaDatos;
+
+namespace CapaNegocios
+{
+    public class BllLocalidades
+    {
+        private DalLocalidades dal = new DalLocalidades();
+        public List<Localidades> ListarTodas()
+        {
+            return dal.ListarTodas();
+        }
+
+        public List<Localidades> GetByIdMunicipio(int IdMunicipio)
+        {
+            return dal.GetByMunicipio(IdMunicipio);
+        }
+    }
+}
